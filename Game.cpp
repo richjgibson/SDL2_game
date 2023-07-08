d5107f56c2c9c7c8cf685b333a83f14a95fa5fd1
Richard Gibson <email@richardgibson.co.uk>
2023-07-08 20:39:09 +0100

    Game class separated

---
+++ Game.cpp
@@ -0,0 +1,65 @@
+#include "Game.h"
+#include <stdio.h>
+
+Game::Game()
+{
+}
+
+Game::~Game()
+{
+}
+
+bool Game::Init(const char *title, int xpos, int ypos, int width, int height)
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    {
+        printf("SDL could not initialize. SDL error: %s\n", SDL_GetError());
+        return false;
+    }
+    if (NULL == (window = SDL_CreateWindow(title, xpos, ypos, width, height, SDL_WINDOW_SHOWN)))
+    {
+        printf("Window not created. SDL error: %s\n", SDL_GetError());
+        return false;
+    }
+    if (NULL == (renderer = SDL_CreateRenderer(window, -1, 0)))
+    {
+        printf("Renderer not created. SDL error: %s\n", SDL_GetError());
+        return false;
+    }
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
+    running = true;
+    return true;
+}
+
+void Game::HandleEvents()
+{
+    SDL_Event e;
+    SDL_PollEvent(&e);
+    switch (e.type)
+    {
+    case SDL_QUIT:
+        running = false;
+        break;
+
+    default:
+        break;
+    }
+}
+
+void Game::Update()
+{
+}
+
+void Game::Render()
+{
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+}
+
+void Game::Quit()
+{
+    printf("Cleaning game...\n");
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+    SDL_Quit();
+}
---
+++ Game.h
@@ -0,0 +1,19 @@
+#include <SDL2/SDL.h>
+
+class Game
+{
+private:
+    SDL_Window *window = nullptr;
+    SDL_Renderer *renderer = nullptr;
+    bool running;
+
+public:
+    Game();
+    ~Game();
+    bool Init(const char *title, int xpos, int ypos, int width, int height);
+    void HandleEvents();
+    void Update();
+    void Render();
+    void Quit();
+    bool gameRunning() { return running; }
+};
