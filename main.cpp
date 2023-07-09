295251c1e1f007c7cf850fbd5c0925c23ef8c15f
Richard Gibson <email@richardgibson.co.uk>
2023-07-09 12:38:42 +0100

    Add constant FPS

--- main.cpp
+++ main.cpp
@@ -4,15 +4,30 @@ Game *game = nullptr;
 
 int main(int argc, char *argv[])
 {
+    const int FPS = 60;
+    const int FRAMEDELAY = 1000 / FPS;
+
+    Uint32 frameStart;
+    int frameTime;
+
     game = new Game();
 
     game->Init("WORKING_TITLE", 0, 0, 640, 480);
 
     while (game->gameRunning())
     {
+        frameStart = SDL_GetTicks();
+
         game->HandleEvents();
         game->Update();
         game->Render();
+
+        frameTime = SDL_GetTicks() - frameStart;
+
+        if (FRAMEDELAY > frameTime)
+        {
+            SDL_Delay(FRAMEDELAY - frameTime);
+        }
     }
 
     game->Quit();
